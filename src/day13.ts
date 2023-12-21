b68abe99e3b9f3e839478909e2424dcf9569a9b3
Jonathan Gruner <jogru0@gmail.com>
2023-12-22 00:45:48 +0100

    day 13

--- src/day13.ts
+++ src/day13.ts
@@ -1,7 +1,100 @@
+function parse(lines: string[]): string[][] {
+  lines.push("");
+  let index = 0;
+  const result: string[][] = [];
+  do {
+    const next = lines.indexOf("", index);
+    result.push(lines.slice(index, next));
+    index = next + 1;
+  } while (index != lines.length);
+
+  lines.pop();
+  return result;
+}
+
+function horizontal(map: string[]): number {
+  let score = 0;
+  for (let i = 0; i < map.length - 1; ++i) {
+    let symmetric = true;
+    const to_check = Math.min(i + 1, map.length - i - 1);
+    for (let j = 0; j < to_check; ++j) {
+      if (map[i - j] != map[i + j + 1]) {
+        symmetric = false;
+        break;
+      }
+    }
+
+    if (symmetric) {
+      score += i + 1;
+    }
+  }
+
+  return score;
+}
+
+function same_up_to_smudge(l: string, r: string): [boolean, boolean] {
+  let smudges = false;
+  for (let i = 0; i - l.length; ++i) {
+    if (l[i] != r[i]) {
+      if (smudges) {
+        return [false, undefined];
+      } else {
+        smudges = true;
+      }
+    }
+  }
+  return [true, smudges];
+}
+
+function horizontal_smudge(map: string[]): number {
+  for (let i = 0; i < map.length - 1; ++i) {
+    let used_smudge = false;
+    let symmetric = true;
+    const to_check = Math.min(i + 1, map.length - i - 1);
+    for (let j = 0; j < to_check; ++j) {
+      const [same, smudged] = same_up_to_smudge(map[i - j], map[i + j + 1]);
+
+      if (!same || (used_smudge && smudged)) {
+        symmetric = false;
+        break;
+      }
+
+      used_smudge ||= smudged;
+    }
+
+    if (symmetric && used_smudge) {
+      return i + 1;
+    }
+  }
+
+  return 0;
+}
+
+function transpose(map: string[]): string[] {
+  if (map.length == 0 || map[0].length == 0) {
+    return [];
+  }
+
+  const result: string[] = [];
+  [...map[0]].forEach(() => result.push(""));
+  map.forEach((line) => [...line].forEach((c, i) => (result[i] += c)));
+  return result;
+}
+
+function get_value(map: string[]): number {
+  return 100 * horizontal(map) + horizontal(transpose(map));
+}
+
+function get_value_2(map: string[]): number {
+  return 100 * horizontal_smudge(map) + horizontal_smudge(transpose(map));
+}
+
 export function part1(lines: string[]): number {
-  return lines.length;
+  const maps = parse(lines);
+  return maps.reduce((sum, map) => sum + get_value(map), 0);
 }
 
 export function part2(lines: string[]): number {
-  return lines.length;
+  const maps = parse(lines);
+  return maps.reduce((sum, map) => sum + get_value_2(map), 0);
 }
