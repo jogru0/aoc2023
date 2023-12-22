b973cdb951debd3573793a8153aa1a551cf0f908
Jonathan Gruner <jogru0@gmail.com>
2023-12-22 17:27:43 +0100

    cleaned up dead code from a previous attempt of day 12

--- src/day12.ts
+++ src/day12.ts
@@ -1,47 +1,6 @@
-function to_sequence(springs: number[]): [number[], number, number] {
-  const result: number[] = [];
-  let last_broken;
-  springs.push(1);
-
-  let s_id = 0;
-  for (; s_id < springs.length; ++s_id) {
-    const n = springs[s_id];
-    if (n == 0 && last_broken === undefined) {
-      last_broken = s_id;
-    } else if (n == 1 && last_broken !== undefined) {
-      result.push(s_id - last_broken);
-      last_broken = undefined;
-    }
-    if (n == -1) {
-      break;
-    }
-  }
-  springs.pop();
-
-  let start_index_count: number;
-  if (last_broken === undefined) {
-    start_index_count = s_id;
-  } else {
-    start_index_count = last_broken;
-  }
-
-  let min = 0;
-  let max = 0;
-  for (let s = start_index_count; s < springs.length; ++s) {
-    if (springs[s] == 0) {
-      ++min;
-    }
-    if (springs[s] != 1) {
-      ++max;
-    }
-  }
-
-  return [result, min, max];
-}
-
 const memo = new Map<string, number>();
 
-function valid2(springs: number[], sequence: number[]): number {
+function valid(springs: number[], sequence: number[]): number {
   const stringified = JSON.stringify([springs, sequence]);
   const memoized = memo.get(stringified);
   if (memoized !== undefined) {
@@ -75,59 +34,13 @@ function valid2(springs: number[], sequence: number[]): number {
     }
 
     if (start_index == 0) {
-      result += valid2([], sequence.slice(0, -1));
+      result += valid([], sequence.slice(0, -1));
     } else {
-      result += valid2(
-        springs.slice(0, start_index - 1),
-        sequence.slice(0, -1)
-      );
+      result += valid(springs.slice(0, start_index - 1), sequence.slice(0, -1));
     }
   }
 
   memo.set(stringified, result);
-  return result;
-}
-
-function valid(springs: number[], sequence: number[]): number {
-  const next_unknown_id = springs.indexOf(-1);
-
-  const [actual_sequence, remaining_min, remaining_max] = to_sequence(springs);
-  if (next_unknown_id == -1) {
-    if (
-      actual_sequence.length == sequence.length &&
-      actual_sequence.every((elem, i) => elem == sequence[i])
-    ) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-
-  if (
-    sequence.length < actual_sequence.length ||
-    actual_sequence.some((elem, i) => elem != sequence[i])
-  ) {
-    return 0;
-  }
-
-  let rem = 0;
-  for (let i = actual_sequence.length; i < sequence.length; ++i) {
-    rem += sequence[i];
-  }
-
-  if (rem < remaining_min) {
-    return 0;
-  }
-
-  if (rem > remaining_max) {
-    return 0;
-  }
-
-  springs[next_unknown_id] = 0;
-  let result = valid(springs, sequence);
-  springs[next_unknown_id] = 1;
-  result += valid(springs, sequence);
-  springs[next_unknown_id] = -1;
   return result;
 }
 
@@ -150,7 +63,7 @@ export function part1(lines: string[]): number {
   });
 
   return springs_sequence_array.reduce(
-    (sum, [springs, sequence]) => sum + valid2(springs, sequence),
+    (sum, [springs, sequence]) => sum + valid(springs, sequence),
     0
   );
 }
@@ -181,16 +94,16 @@ export function part2(lines: string[]): number {
     expanded_strings.push(-1);
     expanded_strings.push(...springs);
 
-    const expamded_sequence = [...sequence];
-    expamded_sequence.push(...sequence);
-    expamded_sequence.push(...sequence);
-    expamded_sequence.push(...sequence);
-    expamded_sequence.push(...sequence);
+    const expanded_sequence = [...sequence];
+    expanded_sequence.push(...sequence);
+    expanded_sequence.push(...sequence);
+    expanded_sequence.push(...sequence);
+    expanded_sequence.push(...sequence);
 
-    return [expanded_strings, expamded_sequence];
+    return [expanded_strings, expanded_sequence];
   });
 
-  return springs_sequence_array.reduce((sum, [springs, sequence], i) => {
-    return sum + valid2(springs, sequence);
+  return springs_sequence_array.reduce((sum, [springs, sequence]) => {
+    return sum + valid(springs, sequence);
   }, 0);
 }
